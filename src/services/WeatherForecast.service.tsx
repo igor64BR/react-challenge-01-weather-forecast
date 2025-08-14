2c6c935183641582c477049e2378260b6678cb8b
Baiocco-iALL <igor.rodrigues@industriall.ai>
2025-08-14 10:52:51 -0300

    feat: Implementação de temp max e min futuras em weather forecast service

--- src/services/WeatherForecast.service.tsx
+++ src/services/WeatherForecast.service.tsx
@@ -70,107 +70,42 @@ const WeatherForecastContextProviderProps = ({
     for (const response of responses) {
       const index = responses.indexOf(response);
 
-      // Attributes for timezone and location
       const latitude = response.latitude();
       const longitude = response.longitude();
-      // const elevation = response.elevation();
-      // const timezone = response.timezone();
-      // const timezoneAbbreviation = response.timezoneAbbreviation();
-      // const utcOffsetSeconds = response.utcOffsetSeconds();
-
-      // console.log(
-      //   `\nCoordinates: ${latitude}°N ${longitude}°E`,
-      //   `\nElevation: ${elevation}m asl`,
-      //   `\nTimezone: ${timezone} ${timezoneAbbreviation}`,
-      //   `\nTimezone difference to GMT+0: ${utcOffsetSeconds}s`
-      // );
 
       const current = response.current()!;
       const daily = response.daily()!;
 
-      // Note: The order of weather variables in the URL query and the indices below need to match!
-      // const weatherData = {
-      //   current: {
-      //     time: new Date((Number(current.time()) + utcOffsetSeconds) * 1000),
-      //     temperature_2m: current.variables(0)!.value(),
-      //     apparent_temperature: current.variables(1)!.value(),
-      //     wind_speed_10m: current.variables(2)!.value(),
-      //     relative_humidity_2m: current.variables(3)!.value(),
-      //   },
-      //   daily: {
-      //     time: [
-      //       ...Array(
-      //         (Number(daily.timeEnd()) - Number(daily.time())) /
-      //           daily.interval()
-      //       ),
-      //     ].map(
-      //       (_, i) =>
-      //         new Date(
-      //           (Number(daily.time()) +
-      //             i * daily.interval() +
-      //             utcOffsetSeconds) *
-      //             1000
-      //         )
-      //     ),
-      //     temperature_2m_max: daily.variables(0)!.valuesArray(),
-      //     temperature_2m_min: daily.variables(1)!.valuesArray(),
-      //   },
-      // };
+      const maxTemps = daily.variables(0)!.valuesArray()!;
+      const minTemps = daily.variables(1)!.valuesArray()!;
+
+      const dailyForecast: dailyForecast[] = [];
+
+      for (let i = 0; i < 4; i++) {
+        const max = maxTemps[i + 1];
+        const min = minTemps[i + 1];
+
+        dailyForecast.push({
+          dayName: "Teste " + (i + 1),
+          max,
+          min,
+        });
+      }
 
       outputData.push({
         latitude: latitude,
         longitude: longitude,
-        min: daily.variables(1)!.valuesArray()![0],
-        max: daily.variables(0)!.valuesArray()![0]!,
+        min: dailyForecast[0].min,
+        max: dailyForecast[0].max,
         name: cities[index].name,
-        apparentTemperature: current.variables(1)!.value(),
-        country: cities[index].country,
         temperature: current.variables(0)!.value(),
-        dailyForecast: [
-          {
-            dayName: "Terça",
-            max: 26,
-            min: 18,
-          },
-          {
-            dayName: "Terça",
-            max: 26,
-            min: 18,
-          },
-          {
-            dayName: "Terça",
-            max: 26,
-            min: 18,
-          },
-          {
-            dayName: "Terça",
-            max: 26,
-            min: 18,
-          },
-        ],
-        humidity: current.variables(3)!.value(),
-        state: cities[index].state,
+        apparentTemperature: current.variables(1)!.value(),
         windSpeed: current.variables(2)!.value(),
+        humidity: current.variables(3)!.value(),
+        country: cities[index].country,
+        dailyForecast,
+        state: cities[index].state,
       });
-
-      // 'weatherData' now contains a simple structure with arrays with datetime and weather data
-      // console.log(
-      //   `\Current data for ${
-      //     [
-      //       {
-      //         name: "Alegre",
-      //         latitude: position.latitude,
-      //         longitude: position.longitude,
-      //       },
-      //       ...cities,
-      //     ][index].name
-      //   }`,
-      //   `\nCurrent time: ${weatherData.current.time}`,
-      //   `\nCurrent temperature_2m: ${weatherData.current.temperature_2m}`,
-      //   `\nCurrent apparent_temperature: ${weatherData.current.apparent_temperature}`,
-      //   `\nCurrent wind_speed_10m: ${weatherData.current.wind_speed_10m}`,
-      //   `\nCurrent relative_humidity_2m: ${weatherData.current.relative_humidity_2m}`
-      // );
     }
 
     setCachedData(outputData);
